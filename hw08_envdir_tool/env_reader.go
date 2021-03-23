75283a4100cfd18fb139eceacba5440822cfc3bd
Denis Veselov <denis.veselov@NB-VESELOV.local>
2021-03-23 21:24:25 +0300

    hw08_envdir_tool

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,16 +1,83 @@
 package main
 
-type Environment map[string]EnvValue
+import (
+	"bufio"
+	"errors"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"unicode"
+)
 
-// EnvValue helps to distinguish between empty files and files with the first empty line.
-type EnvValue struct {
-	Value      string
-	NeedRemove bool
+type Environment map[string]EnvVal
+
+// EnvVal helps to distinguish between empty files and files with the first empty line.
+type EnvVal struct {
+	Value    string
+	UnsetVal bool
 }
+
+var (
+	ErrInvalidFileName  = errors.New("filename contains '=' symbol")
+	ErrFileIsADirectory = errors.New("file is a directory")
+)
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	filesInfo, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	envVars := make(Environment, len(filesInfo))
+	for _, fInfo := range filesInfo {
+		if err := validateFileInfo(fInfo); err != nil {
+			return nil, err
+		}
+
+		file, err := os.Open(filepath.Join(dir, fInfo.Name()))
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		envVars[fInfo.Name()] = filterEnvVal(scanner.Text())
+	}
+
+	return envVars, nil
+}
+
+func validateFileInfo(fInfo fs.FileInfo) error {
+	if fInfo.IsDir() {
+		return ErrFileIsADirectory
+	}
+
+	if strings.Contains(fInfo.Name(), "=") {
+		return ErrInvalidFileName
+	}
+
+	return nil
+}
+
+func filterEnvVal(s string) EnvVal {
+	var envVal EnvVal
+
+	s = strings.TrimRightFunc(s, unicode.IsSpace)
+	s = strings.ReplaceAll(s, string([]byte{'\x00'}), "\n")
+	if len(s) == 0 {
+		envVal.UnsetVal = true
+	}
+
+	envVal.Value = s
+
+	return envVal
 }
