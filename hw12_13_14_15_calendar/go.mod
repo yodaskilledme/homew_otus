b9d0d5cc64f51182eaf108ce18048e225908ca1f
Denis Veselov <denis.veselov@NB-VESELOV.local>
2021-04-21 20:15:39 +0300

    fix cs

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -3,19 +3,13 @@ module github.com/yodaskilledme/homew_otus/hw12_13_14_15_calendar
 go 1.16
 
 require (
-	github.com/ClickHouse/clickhouse-go v1.4.3 // indirect
-	github.com/denisenkom/go-mssqldb v0.9.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/gojuno/minimock/v3 v3.0.8 // indirect
 	github.com/jmoiron/sqlx v1.3.1
 	github.com/lib/pq v1.10.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pressly/goose v2.7.0+incompatible // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/ziutek/mymysql v1.5.4 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
 	golang.org/x/tools v0.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
--- hw12_13_14_15_calendar/migrations/20210413225621_init.sql
+++ hw12_13_14_15_calendar/migrations/20210413225621_init.sql
@@ -1,15 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE events (
-                        id serial NOT NULL,
-                        title varchar(255) NOT NULL,
-                        date_start timestamp NOT NULL,
-                        date_end timestamp NOT NULL,
-                        description text NULL,
-                        user_id int NOT NULL,
-                        created_at timestamp NOT NULL DEFAULT now(),
-                        updated_at timestamp NULL,
-                        CONSTRAINT events_pk PRIMARY KEY (id)
+CREATE TABLE events
+(
+    id          serial       NOT NULL,
+    title       varchar(255) NOT NULL,
+    date_start  timestamp    NOT NULL,
+    date_end    timestamp    NOT NULL,
+    description text         NULL,
+    user_id     int          NOT NULL,
+    created_at  timestamp    NOT NULL DEFAULT now(),
+    updated_at  timestamp    NULL,
+    CONSTRAINT events_pk PRIMARY KEY (id)
 );
 CREATE INDEX events_date_period_idx ON events USING btree (date_start, date_end);
 CREATE UNIQUE INDEX events_user_id_date_start_date_end_idx ON events USING btree (user_id, date_start, date_end);
